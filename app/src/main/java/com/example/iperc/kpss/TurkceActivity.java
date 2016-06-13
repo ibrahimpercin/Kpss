ffdef7a62319d35d05d5296432eab8ad62866a02
ipercin44 <ipercin44@gmail.com>
2016-06-13 12:46:11 +0300

    Main activity Düzenlendi Logolar Eklendi yeni activity oluşturuldu gereksiz kodlar temizlendi

---
+++ app/src/main/java/com/example/iperc/kpss/TurkceActivity.java
@@ -0,0 +1,13 @@
+package com.example.iperc.kpss;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TurkceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_turkce);
+    }
+}
