30a459a47337b43c17e3cee8de2010782c450c5a
ipercin44 <ipercin44@gmail.com>
2016-06-14 09:21:07 +0300

    14/06/2016 Güncellemeler eklendi

--- app/src/main/java/com/example/iperc/kpss/MainActivity.java
+++ app/src/main/java/com/example/iperc/kpss/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.iperc.kpss;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -34,7 +36,11 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
-
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, TurkceActivity.class);
+        startActivity(intent);
+    }
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
