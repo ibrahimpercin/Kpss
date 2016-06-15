8fd892c9d535aec1fe38af6e2fbbe073fe204e65
ipercin44 <ipercin44@gmail.com>
2016-06-15 09:45:18 +0300

    Activtyler düzenlendi Temizleme yapıldı icerikler tek sayfada olacak şekilde yeni activity oluşturuldu
    Yapılacaklar
    Expandableviewlist eklenecek
    Veritabanı baülantılaır oluşturulacak

--- app/src/main/java/com/example/iperc/kpss/icerikActivity.java
+++ app/src/main/java/com/example/iperc/kpss/icerikActivity.java
@@ -3,11 +3,11 @@ package com.example.iperc.kpss;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class TurkceActivity extends AppCompatActivity {
+public class icerikActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_turkce);
+        setContentView(R.layout.activity_icerik);
     }
 }
