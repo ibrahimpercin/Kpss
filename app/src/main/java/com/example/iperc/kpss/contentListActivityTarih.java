2eef881f712ff98cb67145466496f43f8cff7849
ipercin44 <ipercin44@gmail.com>
2016-06-25 10:29:17 +0300

    Tarih dersi içerikleri eklendi

--- app/src/main/java/com/example/iperc/kpss/contentListActivityTarih.java
+++ app/src/main/java/com/example/iperc/kpss/contentListActivityTarih.java
@@ -1,6 +1,7 @@
 package com.example.iperc.kpss;
 
 import android.app.Application;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -85,60 +86,236 @@ public class contentListActivityTarih extends AppCompatActivity {
             @Override
             public boolean onChildClick(ExpandableListView parent, View v,
                                         int groupPosition, int childPosition, long id) {
-                // TODO Auto-generated method stub
-                Toast.makeText(
-                        getApplicationContext(),
-                        listDataHeader.get(groupPosition)
-                                + " : "
-                                + listDataChild.get(
-                                listDataHeader.get(groupPosition)).get(
-                                childPosition), Toast.LENGTH_SHORT)
-                        .show();
-                return false;
+                final String selected = (String) listAdapter.getChild(groupPosition, childPosition);
+                Intent intent;
+                //Seçime göre açılma ve içeriği güncelleme
+                switch(selected){
+                    case "İslamiyet Öncesi Türk tarihi":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_islamiyet_oncesi));
+                        intent.putExtra("icerik2",getString(R.string.tarih_islamiyet_oncesi_title));
+                        startActivity(intent);
+                        break;
+                    case "İlk Türk Devletleri":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_ilkturk_devletleri));
+                        intent.putExtra("icerik2",getString(R.string.tarih_ilkturk_devletleri_title));
+                        startActivity(intent);
+                        break;
+                    case "İslamiyet Öncesi Türk Devletleri Kültür ve Medeniyeti":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_islamiyet_oncesi));
+                        intent.putExtra("icerik2",getString(R.string.tarih_islamiyet_oncesi_title));
+                        startActivity(intent);
+                        break;
+                    case "ilk Türk İslam Devletleri":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_islam_devletleri));
+                        intent.putExtra("icerik2",getString(R.string.tarih_islam_devletleri_title));
+                        startActivity(intent);
+                        break;
+                    case "Selçuklu Devleti Kültür ve Medeniyeti":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_anadolu_selculu_kultur));
+                        intent.putExtra("icerik2",getString(R.string.tarih_anadolu_selculu_kultur_title));
+                        startActivity(intent);
+                        break;
+                    case "Orta Asya Türk Devletleri":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_orta_asya));
+                        intent.putExtra("icerik2",getString(R.string.tarih_orta_asya_title));
+                        startActivity(intent);
+                        break;
+                    case "Türk Beylikleri":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_turk_beylikleri));
+                        intent.putExtra("icerik2",getString(R.string.tarih_turk_beylikleri_title));
+                        startActivity(intent);
+                    case "Anadolu Selçuklu Devleti":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_anadolu_selcuklu));
+                        intent.putExtra("icerik2",getString(R.string.tarih_selcuklu_title));
+                        startActivity(intent);
+                        break;
+                    case "Anadolu Türk Beylikleri":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_turk_beylikleri));
+                        intent.putExtra("icerik2",getString(R.string.tarih_turk_beylikleri_title));
+                        startActivity(intent);
+                        break;
+                    case "Anadolu Selçuklu Devleti Kültür ve Medeniyeti":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_anadolu_selculu_kultur));
+                        intent.putExtra("icerik2",getString(R.string.tarih_anadolu_selculu_kultur_title));
+                        startActivity(intent);
+                        break;
+                    case "Osmanlı İmparatorluğu Kültür ve Medeniyeti – 1":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_osmanlı_kultur1));
+                        intent.putExtra("icerik2",getString(R.string.tarih_osmanlı_kultur1_title));
+                        startActivity(intent);
+                        break;
+                    case "Osmanlı İmparatorluğu Kültür ve Medeniyeti – 2":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_osmanlı_kultur2));
+                        intent.putExtra("icerik2",getString(R.string.tarih_osmanlı_kultur2_title));
+                        startActivity(intent);
+                        break;
+                    case "Osmanlı İmparatorluğu Kültür ve Medeniyeti – 3":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_osmanlı_kultur3));
+                        intent.putExtra("icerik2",getString(R.string.tarih_osmanlı_kultur3_title));
+                        startActivity(intent);
+                        break;
+                    case "Osmanlı İmparatorluğu Kültür ve Medeniyeti – 4":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_osmanlı_kultur4));
+                        intent.putExtra("icerik2",getString(R.string.tarih_osmanlı_kultur4_title));
+                        startActivity(intent);
+                        break;
+                    case "Osmanlı İmparatorluğu Kültür ve Medeniyeti – 5":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_osmanlı_kultur5));
+                        intent.putExtra("icerik2",getString(R.string.tarih_osmanlı_kultur5_title));
+                        startActivity(intent);
+                    case "Osmanlı İmparatorluğu Kültür ve Medeniyeti – 6":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_osmanlı_kultur6));
+                        intent.putExtra("icerik2",getString(R.string.tarih_osmanlı_kultur6_title));
+                        startActivity(intent);
+                        break;
+                    case "Beylikten Devlete Osmanlı (1300-1453)":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_beylikten_osmanlı));
+                        intent.putExtra("icerik2",getString(R.string.tarih_beylikten_osmanlı_title));
+                        startActivity(intent);
+                        break;
+                    case "İstanbul’un Fethi 1453":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_istanbul_fatih));
+                        intent.putExtra("icerik2",getString(R.string.tarih_istanbul_fatih_title));
+                        startActivity(intent);
+                        break;
+                    case "Osmanlı Yükselme Dönemi (1453-1600)":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_osmanlı_yukselis));
+                        intent.putExtra("icerik2",getString(R.string.tarih_osmanlı_yukselis_title));
+                        startActivity(intent);
+                        break;
+                    case "Osmanlı Duraklama Dönemi":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_osmanli_duraklama));
+                        intent.putExtra("icerik2",getString(R.string.tarih_osmanli_duraklama_title));
+                        startActivity(intent);
+                        break;
+                    case "Duraklama Dönemi Dış Nedenleri":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_osmanli_duraklama2));
+                        intent.putExtra("icerik2",getString(R.string.tarih_osmanli_duraklama2_title));
+                        startActivity(intent);
+                    case "17. Yüzyıl İsyanları":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_isyanlar));
+                        intent.putExtra("icerik2",getString(R.string.tarih_isyanlar_title));
+                        startActivity(intent);
+                        break;
+                    case "18. Yüzyıl Islahatları":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_islihatlar));
+                        intent.putExtra("icerik2",getString(R.string.tarih_islihatlar_title));
+                        startActivity(intent);
+                        break;
+                    case "19. Yüzyıl Siyasi Olayları":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_siyasi_olaylar));
+                        intent.putExtra("icerik2",getString(R.string.tarih_siyasi_olaylar_title));
+                        startActivity(intent);
+                        break;
+                    case "Osmanlı Rus Savaşı (1877-1878)":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_osmanli_rus));
+                        intent.putExtra("icerik2",getString(R.string.tarih_osmanli_rus));
+                        startActivity(intent);
+                        break;
+                    case "I. Meşrutiyet":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_mesrutiyet));
+                        intent.putExtra("icerik2",getString(R.string.tarih_mesrutiyet_title));
+                        startActivity(intent);
+                        break;
+                    case "II. Meşrutiyet":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_mesrutiyet2));
+                        intent.putExtra("icerik2",getString(R.string.tarih_mesrutiyet2_title));
+                        startActivity(intent);
+                        break;
+                    case "31 Mart Olayı":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_31Mart));
+                        intent.putExtra("icerik2",getString(R.string.tarih_31Mart_title));
+                        startActivity(intent);
+                        break;
+                    case "Osmanlı Fikir Akımları":
+                        intent = new Intent(contentListActivityTarih.this, icerikViewActivity.class);
+                        intent.putExtra("icerik",getString(R.string.tarih_fikir_akimlari));
+                        intent.putExtra("icerik2",getString(R.string.tarih_fikir_akimlari_title));
+                        startActivity(intent);
+                        break;
+                }
+                return true;
             }
         });
     }
 
-    /*
-     * Preparing the list data
-     */
+    
     private void prepareListData() {
         listDataHeader = new ArrayList<String>();
         listDataChild = new HashMap<String, List<String>>();
 
         // Adding child data
-        listDataHeader.add("Top0");
-        listDataHeader.add("Now Showing");
-        listDataHeader.add("Coming Soon..");
+        listDataHeader.add("İslamiyet Öncesi Türk tarihi");
+        listDataHeader.add("İslam Devletleri");
+        listDataHeader.add("Osmanlı Tarihi");
 
         // Adding child data
-        List<String> top250 = new ArrayList<String>();
-        top250.add("The Shawshank Redemption");
-        top250.add("The Godfather");
-        top250.add("The Godfather: Part II");
-        top250.add("Pulp Fiction");
-        top250.add("The Good, the Bad and the Ugly");
-        top250.add("The Dark Knight");
-        top250.add("12 Angry Men");
+        List<String> islamiyet_once = new ArrayList<String>();
+        islamiyet_once.add("İslamiyet Öncesi Türk Tarihi");
+        islamiyet_once.add("İlk Türk Devletleri");
+        islamiyet_once.add("İslamiyet Öncesi Türk Devletleri Kültür ve Medeniyeti");
 
-        List<String> nowShowing = new ArrayList<String>();
-        nowShowing.add("The Conjuring");
-        nowShowing.add("Despicable Me 2");
-        nowShowing.add("Turbo");
-        nowShowing.add("Grown Ups 2");
-        nowShowing.add("Red 2");
-        nowShowing.add("The Wolverine");
+        List<String> islam_dev = new ArrayList<String>();
+        islam_dev.add("ilk Türk İslam Devletleri");
+        islam_dev.add("Selçuklu Devleti Kültür ve Medeniyeti");
+        islam_dev.add("Orta Asya Türk Devletleri");
+        islam_dev.add("Türk Beylikleri");
+        islam_dev.add("Anadolu Selçuklu Devleti");
+        islam_dev.add("Anadolu Türk Beylikleri");
+        islam_dev.add("Anadolu Selçuklu Devleti Kültür ve Medeniyeti");
 
-        List<String> comingSoon = new ArrayList<String>();
-        comingSoon.add("2 Guns");
-        comingSoon.add("The Smurfs 2");
-        comingSoon.add("The Spectacular Now");
-        comingSoon.add("The Canyons");
-        comingSoon.add("Europa Report");
-
-        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
-        listDataChild.put(listDataHeader.get(1), nowShowing);
-        listDataChild.put(listDataHeader.get(2), comingSoon);
+        List<String> osmanli_dev = new ArrayList<String>();
+        osmanli_dev.add("Osmanlı İmparatorluğu Kültür ve Medeniyeti – 1");
+        osmanli_dev.add("Osmanlı İmparatorluğu Kültür ve Medeniyeti – 2");
+        osmanli_dev.add("Osmanlı İmparatorluğu Kültür ve Medeniyeti – 3");
+        osmanli_dev.add("Osmanlı İmparatorluğu Kültür ve Medeniyeti – 4");
+        osmanli_dev.add("Osmanlı İmparatorluğu Kültür ve Medeniyeti – 5");
+        osmanli_dev.add("Osmanlı İmparatorluğu Kültür ve Medeniyeti – 6");
+        osmanli_dev.add("Beylikten Devlete Osmanlı (1300-1453)");
+        osmanli_dev.add("İstanbul’un Fethi 1453");
+        osmanli_dev.add("Osmanlı Yükselme Dönemi (1453-1600)");
+        osmanli_dev.add("Osmanlı Duraklama Dönemi");
+        osmanli_dev.add("Duraklama Dönemi Dış Nedenleri");
+        osmanli_dev.add("17. Yüzyıl İsyanları");
+        osmanli_dev.add("18. Yüzyıl Islahatları");
+        osmanli_dev.add("19. Yüzyıl Siyasi Olayları");
+        osmanli_dev.add("Osmanlı Rus Savaşı (1877-1878)");
+        osmanli_dev.add("I. Meşrutiyet");
+        osmanli_dev.add("II. Meşrutiyet");
+        osmanli_dev.add("31 Mart Olayı");
+        osmanli_dev.add("Osmanlı Fikir Akımları");
+        
+        listDataChild.put(listDataHeader.get(0), islamiyet_once); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), islam_dev);
+        listDataChild.put(listDataHeader.get(2), osmanli_dev);
     }
     public class MainApplication extends Application {
         @Override
